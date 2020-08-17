e8ab64b8519f5aeddd26dafe427ebf27ff883267
jejune5 <752023928@qq.com>
2020-08-17 14:09:39 +0800

    update

--- HAO/二叉树/102 层次遍历与BFS.py
+++ HAO/二叉树/102 层次遍历与BFS.py
@@ -1,15 +1,31 @@
 import HAO.二叉树.str2TreeNode as st
+root = st.TreeNode
 
-s = '[1,null,2,null,3,null,4,null,null]'
+
+
+def dfs(root, level, res:[]):
+    if root == None:
+        return res
+
+    if len(res) == level:
+        res.append([root.val])
+    else:
+        res[level].append(root.val)
+
+    res = dfs(root.left, level+1, res)
+    res = dfs(root.right, level+1, res)
+    return res
+
+def levelOrder(root) :
+
+    return dfs(root, 0, res=[])
+
+
+# s = '[3,9,20,null,null,15,7]'
+s = '[]'
 root = st.stringToTreeNode(s)
-res = st.treeNodeToString(root)
+# res = st.treeNodeToString(root)
+
+res = levelOrder(root)
+
 print(res)
-
-a = list(range(1,10))
-a.pop()
-
-# a.p
-
-print(a)
-
-
---
+++ Test/test.py
@@ -0,0 +1,9 @@
+t = ['aA','B','A','b','1']
+
+tt = sorted(t)
+print(__name__)
+print(tt)
+def main():
+    print('main')
+if __name__ == '__main__':
+    main()
