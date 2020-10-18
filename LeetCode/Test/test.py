f42979b9a12f5143159302dd1d4c35cba2c2a1d9
jejune5 <752023928@qq.com>
2020-10-18 11:09:38 +0800

    Update

--- LeetCode/Test/test.py
+++ LeetCode/Test/test.py
@@ -1,15 +1,3 @@
-n=4#int()
-a=[2,1,3,4]#[int(i) for i in input().split()]
 
-b=sorted(a)
-c=[a[i] for i in range(n) if a[i]!=b[i]]
-d=[b[i] for i in range(n) if a[i]!=b[i]]
-c = c[::-1]
-if len(c)>0 and c==d:
-    print("yes")
-else:
-    print("no")
-
-
-t = list('qwererty')
-print(t)
+for x, y in [(2,3), (4,5), (6,7)]:
+    print(x,y)
--- 剑指Offer2/CH2/12 矩阵中的路径.py
+++ 剑指Offer2/CH2/12 矩阵中的路径.py
@@ -1,2 +1,31 @@
 def exist(board, word):
-    pass
+    def isValid(i, j):
+        return 0 <= i < m and 0 <= j < n
+
+    def DFS(i, j, seen):
+        if len(seen) >= len(word):
+            return True
+        for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+            x, y = i + dx, j + dy
+            if isValid(x, y) and word[len(seen)] == board[x][y] and (x, y) not in seen:
+                seen.add((x, y))
+                if DFS(x, y, seen):
+                    return True
+                seen.discard((x, y))
+        return False
+
+    m, n = len(board), len(board[0])
+    for i in range(m):
+        for j in range(n):
+            if board[i][j] == word[0] and DFS(i, j, {(i, j)}):
+                return True
+    return False
+
+
+################### Test ########################
+board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]
+word = "ABCCED"
+
+
+ans = exist(board, word)
+print(ans)
