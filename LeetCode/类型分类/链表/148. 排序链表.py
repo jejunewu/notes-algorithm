979e82d714bc6030d190d650a4e185cbda47b6d1
jejune5 <752023928@qq.com>
2020-11-21 17:08:13 +0800

    update

---
+++ LeetCode/类型分类/链表/148. 排序链表.py
@@ -0,0 +1,29 @@
+from myDataStructure import ListNode
+
+def sortList(head):
+
+    pos = head
+    arr = []
+    while pos:
+        arr.append(pos.val)
+        pos = pos.next
+    arr.sort()
+    print(arr)
+
+    a = ListNode.ListNode(0)
+    b = a
+    for e in arr:
+        b.next = ListNode.ListNode(e)
+        b = b.next
+    b = a.next
+    return b
+
+################### Test ########################
+
+head = [8,6,5,7,6,7,5]
+head = ListNode.list2ListNode(head)
+
+ans = sortList(head)
+ans = ListNode.listNode2List(ans)
+print(ans)
+
