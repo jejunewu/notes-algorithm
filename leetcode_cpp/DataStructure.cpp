9b05c1a15295dbd36867646dcee979a54a5ed690
jejune5 <752023928@qq.com>
2023-08-24 09:57:40 +0800

    update

--- leetcode_cpp/DataStructure.cpp
+++ leetcode_cpp/DataStructure.cpp
@@ -7,15 +7,61 @@
 
 using namespace std;
 
-std::vector<int> listNodeToArray(ListNode *head) {
-    std::vector<int> result;
-    while (head != nullptr) {
-        result.push_back(head->val);
-        head = head->next;
+/**
+ * 将数组转换为 ListNode
+ * @param arr
+ * @param n
+ * @return
+ */
+ListNode *vector2Node(int arr[], int n) {
+    if (n == 0) {
+        return nullptr;
     }
-    return result;
+    ListNode *head = new ListNode(arr[0]);
+    ListNode *cur = head;
+    for (int i = 1; i < n; i++) {
+        cur->next = new ListNode(arr[i]);
+        cur = cur->next;
+    }
+    return head;
 }
 
+/**
+ * 将 ListNode 转为 数组
+ * @param head
+ * @return
+ */
+vector<int> node2Vector(ListNode *head) {
+    ListNode *cur = head;
+    vector<int> arr;
+    while (cur != nullptr) {
+        arr.push_back(cur->val);
+        cur = cur->next;
+    }
+    return arr;
+}
+
+/**
+ * 打印 ListNode 到控制台
+ */
+void printNode(ListNode *head) {
+    ListNode *cur = head;
+    while (cur != nullptr) {
+        cout << cur->val << " -> ";
+        cur = cur->next;
+    }
+    cout << "null" << endl;
+}
+
+
 int main() {
-    ListNode node;
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    ListNode *node = vector2Node(arr, n);
+    printNode(node);
+    vector<int> res = node2Vector(node);
+    for (int n: res) {
+        cout << n;
+    }
+
 }
--- leetcode_cpp/DataStructure.h
+++ leetcode_cpp/DataStructure.h
@@ -5,6 +5,9 @@
 #ifndef NOTES_ALGORITHM_DATASTRUCTURE_H
 #define NOTES_ALGORITHM_DATASTRUCTURE_H
 
+/**
+ * 链表结构
+ */
 struct ListNode {
     int val;
     ListNode *next;
@@ -15,5 +18,12 @@ struct ListNode {
 
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
+
+
+ListNode *vector2Node(int arr[], int n);
+
+std::vector<int> node2Vector(ListNode *head);
+
+void printNode(ListNode *head);
 
 #endif //NOTES_ALGORITHM_DATASTRUCTURE_H
