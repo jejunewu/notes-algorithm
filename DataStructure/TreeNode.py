cd3de8d3935448b0e98fb89f753f22dc061ceded
jejune5 <752023928@qq.com>
2023-08-25 09:54:08 +0800

    update

--- DataStructure/TreeNode.py
+++ DataStructure/TreeNode.py
@@ -1,8 +1,51 @@
+from typing import List
+
+
 class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def build_tree(preorder: List[int]) -> TreeNode:
+    if not preorder:
+        return None
+    root = TreeNode(preorder[0])
+    stack = [root]
+    for value in preorder[1:]:
+        if value is None:
+            continue
+        node = TreeNode(value)
+        if value < stack[-1].val:
+            stack[-1].left = node
+        else:
+            while stack and stack[-1].val < value:
+                last = stack.pop()
+            last.right = node
+        stack.append(node)
+    return root
+
+
+from collections import deque
+
+
+def print_tree(root: TreeNode):
+    if not root:
+        print('Empty tree')
+        return
+    q = deque([root])
+    while q:
+        level = []
+        for _ in range(len(q)):
+            node = q.popleft()
+            if node:
+                level.append(str(node.val))
+                q.append(node.left)
+                q.append(node.right)
+            else:
+                level.append(' ')
+        print(' '.join(level))
 
 
 def list2TreeNode(inputValues):
@@ -57,3 +100,13 @@ def treeNode2List(root):
         queue.append(node.left)
         queue.append(node.right)
     return output
+
+
+if __name__ == '__main__':
+    arr = [1, 2, 3, 4, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    arr = [1, 2, None, 2, 3]
+    # node = build_tree(arr)
+
+    # node = TreeNode(1)
+    # print_tree(node)
+    # print(node)
---
+++ leetcode_py/94-m-treenode-二叉树的中序遍历.py
@@ -0,0 +1,63 @@
+"""
+94. 二叉树的中序遍历
+简单
+1.9K
+相关企业
+给定一个二叉树的根节点 root ，返回 它的 中序 遍历 。
+
+
+
+示例 1：
+
+
+输入：root = [1,null,2,3]
+输出：[1,3,2]
+示例 2：
+
+输入：root = []
+输出：[]
+示例 3：
+
+输入：root = [1]
+输出：[1]
+
+
+提示：
+
+树中节点数目在范围 [0, 100] 内
+-100 <= Node.val <= 100
+
+
+进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+
+"""
+
+from typing import Optional, List
+from DataStructure.TreeNode import *
+
+
+class Solution:
+    # 递归求解
+    def inorder(self, root: Optional[TreeNode], res: list):
+        if root is None:
+            return
+        self.inorder(root.left, res)
+        res.append(root.val)
+        self.inorder(root.right, res)
+
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        self.inorder(root, res)
+        return res
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    cases = [
+        (TreeNode([1, None, 2, 3]), [1, 3, 2]),
+        (TreeNode([]), []),
+        (TreeNode([1]), [1]),
+    ]
+    for idx, case in enumerate(cases):
+        res = list2TreeNode(sol.inorderTraversal(case[0]))
+        print(f"case_id: {idx} || {case[1] == res} || {case[1]} || {res} ")
