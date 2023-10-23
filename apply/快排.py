b85fee9b70759f2661850a4a809fc3d84059a348
jejune5 <752023928@qq.com>
2023-10-23 23:02:30 +0800

    Update 快排.py

---
+++ apply/快排.py
@@ -0,0 +1,33 @@
+"""
+原理参考参考视频教程：
+https://www.bilibili.com/video/BV1vP411g7J3
+
+"""
+
+
+## 原版
+def partition(arr, low, high):
+    pivot = arr[low]
+    while low < high:
+        while low < high and arr[high] >= pivot:
+            high -= 1
+        arr[low] = arr[high]
+        while low < high and arr[low] <= pivot:
+            low += 1
+        arr[high] = arr[low]
+    arr[low] = pivot
+    return low
+
+
+def quick_sort(arr, low, high):
+    if low >= high:
+        return
+    pivotpos = partition(arr, low, high)
+    quick_sort(arr, low, pivotpos - 1)
+    quick_sort(arr, pivotpos + 1, high)
+
+
+if __name__ == '__main__':
+    arr = [2, 9, 7, 3, 4, 6, 8, 1, 0, 5]
+    quick_sort(arr, 0, len(arr) - 1)
+    print(arr)
