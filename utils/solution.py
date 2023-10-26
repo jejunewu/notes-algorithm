0af64a5196e76488a3cec7b73fbd0f2d73d75b73
jejune5 <752023928@qq.com>
2023-10-26 10:10:54 +0800

    update

---
+++ utils/solution.py
@@ -0,0 +1,15 @@
+def solve_batch(solution_obj, cases):
+    """
+    批量测试
+    """
+    sol = solution_obj()
+    fn_str = [func for func in dir(sol) if not func.startswith('__')][-1]
+    fn = getattr(sol, fn_str)
+    for i, case in enumerate(cases):
+        result = fn(*case[:-1])
+        label = case[-1]
+        info = f"case:{i} || {result == label} || {result=} || {label=} ||"
+        for param in case[:-1]:
+            info += f"{param}, "
+        info += f"{result}"
+        print(info)
