3d2a7dc6ef5202a8823626645457462749ed6dce
jejune5 <752023928@qq.com>
2023-11-08 09:46:48 +0800

    Create 普通数组-53-m-最大子数组和.py

---
+++ leetcode_py/hot100/普通数组-53-m-最大子数组和.py
@@ -0,0 +1,61 @@
+"""
+53. 最大子数组和
+中等
+6.4K
+相关企业
+给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+
+子数组 是数组中的一个连续部分。
+
+
+
+示例 1：
+
+输入：nums = [-2,1,-3,4,-1,2,1,-5,4]
+输出：6
+解释：连续子数组 [4,-1,2,1] 的和最大，为 6 。
+示例 2：
+
+输入：nums = [1]
+输出：1
+示例 3：
+
+输入：nums = [5,4,-1,7,8]
+输出：23
+
+
+提示：
+
+1 <= nums.length <= 105
+-104 <= nums[i] <= 104
+"""
+
+from typing import List
+
+"""
+前缀最大和比较
+"""
+
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        pre_max_sum = 0
+        max_sum = nums[0]
+        for n in nums:
+            pre_max_sum = max(pre_max_sum + n, n)
+            max_sum = max(max_sum, pre_max_sum)
+        return max_sum
+
+if __name__ == '__main__':
+    from utils.solution import solve_batch
+
+    cases = [
+        ([-2, 1, -3, 4, -1, 2, 1, -5, 4], 6),
+        ([5, 4, -1, 7, 8], 23),
+        ([1], 1)
+    ]
+
+    solve_batch(
+        Solution,
+        cases,
+    )
