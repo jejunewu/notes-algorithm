468fb13f1455762df9b1511e2a32195bba076476
Jejune <752023928@qq.com>
2025-05-27 10:05:06 +0800

    Create 动态规划-m-152. 乘积最大子数组.py

---
+++ leetcode_py/hot100_second/动态规划-m-152. 乘积最大子数组.py
@@ -0,0 +1,64 @@
+"""
+152. 乘积最大子数组
+已解答
+中等
+相关标签
+相关企业
+给你一个整数数组 nums ，请你找出数组中乘积最大的非空连续 子数组（该子数组中至少包含一个数字），并返回该子数组所对应的乘积。
+
+测试用例的答案是一个 32-位 整数。
+
+
+
+示例 1:
+
+输入: nums = [2,3,-2,4]
+输出: 6
+解释: 子数组 [2,3] 有最大乘积 6。
+示例 2:
+
+输入: nums = [-2,0,-1]
+输出: 0
+解释: 结果不能为 2, 因为 [-2,-1] 不是子数组。
+
+
+提示:
+
+1 <= nums.length <= 2 * 104
+-10 <= nums[i] <= 10
+nums 的任何子数组的乘积都 保证 是一个 32-位 整数
+
+
+"""
+
+from utils import *
+from loguru import logger
+
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = list(nums)
+        for i in range(1, n):
+            # j
+            # if dp[i - 1] * nums[i] > dp[i - 1]:
+            #     dp[i] = dp[i - 1] * nums[i]
+            # else:
+            #     dp[i] = nums[i]
+            # dp[i] = max(dp[i - 1], dp[i - 1] * dp[i], dp[i])
+            # for j in range(i):
+            #     dp[i] = max(dp[j], dp[j] * nums[i], nums[i])
+            logger.info(f"{i=} || {dp=}")
+        return max(dp)
+
+
+if __name__ == '__main__':
+    from utils.solution import solve_batch
+
+    solve_batch(
+        Solution, [
+            ([-2, 3, -4], 24),
+            # ([2, 3, -2, 4], 6),
+            # ([-2, 0, -1], 0),
+        ]
+    )
